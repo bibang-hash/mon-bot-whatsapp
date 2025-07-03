de5413facf85ae62f4216fb26b2b4485357304ab
bibang-hash <kevinbibang355@gmail.com>
2025-07-03 12:16:40 +0000

    Ajout du Procfile et mise à jour du code

--- app.py
+++ app.py
@@ -312,4 +312,5 @@ def serve_pdf(filename):
     return send_from_directory('bons_livraison', filename)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
